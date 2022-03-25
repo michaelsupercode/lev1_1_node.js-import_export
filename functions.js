c8f984a3ae64294252358205e7d63853ee34fd80
SuperCoder <supercoder@AirvonSerCoder3.fritz.box>
2022-03-25 14:22:18 +0100

    first com

---
+++ functions.js
@@ -0,0 +1,11 @@
+function sortCars(cars) {
+    return cars.sort()
+}
+
+function sortNumb(nums) {
+    return nums.sort()
+}
+module.exports = {
+    sortCars,
+    sortNumb
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const { sortCars, sortNumb } = require('./functions')
+const { arrayCars, arrayNumbers } = require('./data')
+console.log(sortCars(arrayCars))
+console.log(sortNumb(arrayNumbers))
